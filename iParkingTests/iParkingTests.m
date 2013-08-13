89ab6cd88a4b8f166f17dd2f67e3fd5d85c4307d
Ramil Garaev <ramilgaraev@libr-3.i.ksu.ru>
2013-08-12 19:07:30 -0700

    Initial Commit

---
+++ iParkingTests/iParkingTests.m
@@ -0,0 +1,34 @@
+//
+//  iParkingTests.m
+//  iParkingTests
+//
+//  Created by Ramil Garaev on 12.08.13.
+//  Copyright (c) 2013 Ramil Garaev. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iParkingTests : XCTestCase
+
+@end
+
+@implementation iParkingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
