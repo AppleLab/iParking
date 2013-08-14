d8543e20b582a8502279ff683f08935d5c466efd
Muzzja <user@s7c-2.i.ksu.ru>
2013-08-14 02:15:49 -0700

    chaki

--- iParking/ViewController.h
+++ iParking/ViewController.h
@@ -7,8 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    MKMapView *mkMapView;
+}
 
 
 
