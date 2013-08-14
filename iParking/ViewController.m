1bd2e86817a32e148ba86b40d10dd43d7f5e6c0c
user <user@Mac-mini-administrator-2.local>
2013-08-14 10:15:35 +0400

    Добавил небольшой интерфейс

--- iParking/ViewController.m
+++ iParking/ViewController.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [[Core core] test];
+    //[[Core core] test];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
