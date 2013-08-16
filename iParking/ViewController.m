93f74d52a1b2a116621bd943e348e1b37d9672d2
user <user@Mac-mini-administrator-6.local>
2013-08-16 15:44:16 +0400

    Добавила метки

--- iParking/ViewController.m
+++ iParking/ViewController.m
@@ -85,6 +85,50 @@
     annotation2.coordinate = CLLocationCoordinate2DMake(47.298674f, 35.395776f);
     [map addAnnotation:annotation2];
     
+    Annotation *annotationK1= [Annotation new];
+    annotationK1.title = @"Гипермаркет «КЦ «Кей Казань»";
+    annotationK1.subtitle = @"Пр.Ибрагимова, 83";
+    annotationK1.coordinate = CLLocationCoordinate2DMake(55.82198352211786, 49.09895181655884);
+    [map addAnnotation:annotationK1];
+    
+    Annotation *annotationK2 = [Annotation new];
+    annotationK2.title = @"Спортивный комплекс «Ватан»";
+    annotationK2.subtitle = @"Ул.Бондаренко, 2";
+    annotationK2.coordinate = CLLocationCoordinate2DMake(55.83370219701798, 49.10495661199093);
+    [map addAnnotation:annotationK2];
+    
+    Annotation *annotationK3 = [Annotation new];
+    annotationK3.title = @"ТК «ДОМО»";
+    annotationK3.subtitle = @"Ул.Декабристов, 113";
+    annotationK3.coordinate = CLLocationCoordinate2DMake(55.821670111900005, 49.08721446990967);
+    [map addAnnotation:annotationK3];
+    
+    Annotation *annotationK4 = [Annotation new];
+    annotationK4.title = @"Бизнес центр";
+    annotationK4.subtitle = @"Ул.Проточная, 8";
+    annotationK4.coordinate = CLLocationCoordinate2DMake(55.81337587801879, 49.08612012863159);
+    [map addAnnotation:annotationK4];
+    
+    //нужно проверить адрес
+    Annotation *annotationK5 = [Annotation new];
+    annotationK5.title = @"Бизнес центр «Союз»";
+    annotationK5.subtitle = @"Ул.Васильченко, 1";
+    annotationK5.coordinate = CLLocationCoordinate2DMake(55.83861851453064, 49.04439568519592);
+    [map addAnnotation:annotationK5];
+    
+    Annotation *annotationK6 = [Annotation new];
+    annotationK6.title = @"Бизнес центр»";
+    annotationK6.subtitle = @"Пр.Ибрагимова, 58";
+    annotationK6.coordinate = CLLocationCoordinate2DMake(55.820100009246715, 49.09582167863846);
+    [map addAnnotation:annotationK6];
+    
+    Annotation *annotationK7 = [Annotation new];
+    annotationK7.title = @"ТК «Мега Мебель»»";
+    annotationK7.subtitle = @"Ул.Сибирский тракт, 34";
+    annotationK7.coordinate = CLLocationCoordinate2DMake(55.81788488144342, 49.18278694152832);
+    [map addAnnotation:annotationK7];
+    
+    
    /* mkMapView = [[MKMapView alloc]initWithFrame: self.view.bounds];
     [self.view addSubview:mkMapView];
     */
