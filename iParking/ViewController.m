52ab85e131840934407d9063e3b8039ea3612022
Muzzja <user@s7c-2.i.ksu.ru>
2013-08-20 14:26:53 +0400

    chekcinformation and delete comment

--- iParking/ViewController.m
+++ iParking/ViewController.m
@@ -45,48 +45,19 @@
 
 }
 
-//initWithPlacemark
 
 
-//-(void) tabView{
-//    int typesWidth=260, typesHeight=30, distanceFromBottom=60;
-//    mapTypes = new UISegmentedControl(new RectangleF((View.Bounds.Width-typesWidth)/2, View.Bounds.Height-distanceFromBottom, typesWidth, typesHeight));
-//    mapTypes.InsertSegment("Road", 0, false);
-//    mapTypes.InsertSegment("Satellite", 1, false);
-//    mapTypes.InsertSegment("Hybrid", 2, false);
-//    mapTypes.SelectedSegment = 0; // Road is the default
-//    mapTypes.AutoresizingMask = UIViewAutoresizing.FlexibleTopMargin;
-//    mapTypes.ValueChanged += (s, e) => {
-//        switch(mapTypes.SelectedSegment) {
-//            case 0:
-//                mapView.MapType = MKMapType.Standard;
-//                break;
-//            case 1:
-//                mapView.MapType = MKMapType.Satellite;
-//                break;
-//            case 2:
-//                mapView.MapType = MKMapType.Hybrid;
-//                break;
-//        }
-//    };
-//}
+
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self GetMyLocation:nil];
-//      //NSDictionary *addressDict = @{MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving};
-//    MKPlacemark * myPlacemark = [[MKPlacemark alloc]initWithCoordinate:CLLocationCoordinate2DMake(55.74762259387401, 49.21452283859253) addressDictionary:nil];
-//    MKMapItem *mapItem = [[MKMapItem alloc] initWithPlacemark:myPlacemark];
-//    mapItem.name=@"Супермаркет «Бэхетле»";
-    /*
-     NSArray* mapitems = @[point1,point2,point3];
-     [MKMapItems openMapWithItems:mapitems launchOptions:nil];
-     */
-    /*NSDictionary *launchOptions = @{MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving};
-    MKMapItem *currentLocationMapItem = [MKMapItem mapItemForCurrentLocation];
-    [MKMapItem openMapsWithItems:@[currentLocationMapItem, mapItem]launchOptions:launchOptions];
-     */
+    //[self GetMyLocation:nil];
+    CLLocationCoordinate2D startCoord = CLLocationCoordinate2DMake(55.779215877174096, 49.129743576049805);
+    MKCoordinateRegion adjustedRegion = [main_map regionThatFits:MKCoordinateRegionMakeWithDistance(startCoord, 20000 , 20000)];
+    [main_map setRegion:adjustedRegion animated:YES];
+
     [self MapToAnnotation];
     
     
